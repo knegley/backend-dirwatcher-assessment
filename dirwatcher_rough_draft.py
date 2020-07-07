72593e0566b502d97555685d0dd0ccf3e427b4ab
knegley <kpnegley@gmail.com>
2020-07-07 03:08:42 -0400

    finished tweaking log file to not repeat itself, need to continue to test program before submitting

--- dirwatcher_rough_draft.py
+++ dirwatcher_rough_draft.py
@@ -29,13 +29,13 @@ def namespace(func):
     directory = args.directory
     magic_string = args.magic
     extension = args.extension
-    print(directory)
-    print(magic_string)
+    # print(directory)
+    # print(magic_string)
     return lambda: func(polling=polling, directory=directory, extension=extension, magic_string=magic_string)
 
 
 @namespace
-async def stream_handler(directory, magic_string, extension, **kwargs) -> "async generator coroutine":
+async def stream_handler(directory: str, magic_string: str, extension: str, **kwargs) -> "async generator coroutine":
     """receives files and directories to be consumed"""
 
     try:
@@ -43,7 +43,9 @@ async def stream_handler(directory, magic_string, extension, **kwargs) -> "async
         with os.scandir(directory) as d:
             file_bank_initial = [f.name for f in d if os.path.isfile(f)]
     except FileNotFoundError as e:
-        logger.exception("Directory does not exist")
+        logger.exception(e)
+    except NotADirectoryError as ex:
+        logger.exception(ex)
 
     while True:
 
@@ -68,35 +70,39 @@ async def stream_handler(directory, magic_string, extension, **kwargs) -> "async
                     f.name for f in direct if os.path.isfile(f)]
 
             f = os.path.basename(output)
-            print(f)
+            # print(f"file : {f}")
 
             is_added = f not in file_bank_initial and f in file_bank_current
 
             # for item in file_bank_current:
             if is_added:
-                print(f"file: {f} added")
+                # print(f"file: {f} added")
+                logger.info(f"file: {f} added")
                 file_bank_initial.append(f)
 
             for z in file_bank_initial:
                 is_removed = z not in file_bank_current
                 if is_removed:
-                    print(f"file: {z} removed")
+                    # print(f"file: {z} removed")
+                    logger.info(f"file: {z} removed")
                     file_bank_initial = [
                         item for item in file_bank_initial if item != z]
 
         #############################################################
 
-            logger.info(f)
+            # logger.info(f)
             # print(os.path.basename(output))
             with open(f) as fil:
                 lines = fil.read().splitlines()
-                print(lines)
+                # print(lines)
             for line_numb, line in enumerate(lines):
+                with open("dirwatcher_rough.log") as dir_log:
+                    log_lines = dir_log.read()
                 if magic_string in line:
-                    print(
-                        f"Found: {magic_string} in File:{f} on line {line_numb}")
-                    logger.info(
-                        f"Found: {magic_string} in File:{f} on line {line_numb}")
+
+                    if f"Found: {magic_string} in File:{f} on line {line_numb}" not in log_lines:
+                        logger.info(
+                            f"Found: {magic_string} in File:{f} on line {line_numb}")
 
         else:
             pass
@@ -111,7 +117,7 @@ async def infiniteloop(polling, directory, **kwargs) -> None:
     await x.asend(None)
 
     while True:
-        print("starting")
+        # print("starting")
         try:
             with os.scandir(directory) as d:
 
@@ -123,30 +129,36 @@ async def infiniteloop(polling, directory, **kwargs) -> None:
         except FileNotFoundError as e:
             continue
 
+        except NotADirectoryError as ex:
+            logger.exception(ex)
+
         except StopIteration:
             continue
         except StopAsyncIteration:
             continue
         finally:
-            print("ended cycle")
+            # print("ended cycle")
             await asyncio.sleep(polling)
 
 
 def exit_program(sig: str, loop: str, /) ->None:
     """receives os signal and handles int and term and ends the loop"""
-    print(f"\nReceived {sig}: exiting...")
+    logger.warning(f"Received {sig}: exiting...")
 
     loop.stop()
 
 
 async def main():
-    logger.info(f"Started at {(time:=datetime.datetime.now())}")
+    """searches for magic text and monitors files being added or removed in a directory"""
+    print("watching...")
+    logger.info("Started")
+    # logger.info(f"Started at {(time:=datetime.datetime.now())}")
     logger.info(f"{(pid:=os.getpid())=}")
     loop = asyncio.get_running_loop()
     sig_names = {"SIGINT", "SIGTERM"}
 
     for sig_name in sig_names:
-        logger.info(f"Signals: {getattr(signal, sig_name)}")
+        logger.info(f"Signal to stop program: {getattr(signal, sig_name)}")
         loop.add_signal_handler(getattr(signal, sig_name),
                                 exit_program, sig_name, loop)
 
@@ -156,6 +168,6 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main(), debug=True)
+        asyncio.run(main())
     except RuntimeError:
-        print("exited program")
+        logger.info("exited program")
